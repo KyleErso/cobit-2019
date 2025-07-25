0beab4fddc21d1b1c2fc0750f9ebfa74d48f2736
[pol] Muhammad Rasheed Qais Tandjung <13522158@std.stei.itb.ac.id>
2025-07-25 16:31:16 +0700

    feat: handle reject login if account is deactivated

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,12 +26,17 @@ class LoginController extends Controller
      */
     protected function authenticated(Request $request, $user)
     {
+        if (!$user->isActivated) {
+            Auth::logout();
+
+            return redirect()->route('login')
+                ->withErrors(['email' => 'Akun Anda telah dinonaktifkan. Silakan hubungi admin.']);
+        }
+
         if ($user->role === 'admin' || $user->role === 'pic') {
-            // jika admin, ke admin dashboard
             return redirect()->route('admin.dashboard');
         }
 
-        // selain admin (user biasa / guest), ke home atau join page
         return redirect()->route('home');
     }
 
@@ -49,31 +54,24 @@ class LoginController extends Controller
 
     public function handleGoogleCallback()
     {
-        // Dapatkan informasi user dari Google
         $googleUser = Socialite::driver('google')->stateless()->user();
-
-        // Cek apakah user sudah ada di database
         $user = User::where('email', $googleUser->getEmail())->first();
 
-        // $user = User::firstOrCreate(
-        //     ['email' => $googleUser->getEmail()],
-        //     [
-        //         'name' => $googleUser->getName(),
-        //         'password' => bcrypt(str()->random(24)), // Password random karena tidak digunakan
-        //         'jabatan' => "Belum ada jabatan"
-        //     ]
-        // );
-
-        
         if (!$user) {
             return view('auth.register-google', [
                 'name' => $googleUser->getName(),
                 'email' => $googleUser->getEmail(),
-                'password' => bcrypt(str()->random(24)), // Password random karena tidak digunakan
+                'password' => bcrypt(str()->random(24)), // Random because not used
             ]);
+        }
+
+        if (!$user->isActivated) {
+            return redirect()->route('login')
+                ->withErrors(['email' => 'Akun Anda telah dinonaktifkan. Silakan hubungi admin.']);
         }
 
         Auth::login($user);
         return redirect('/home');
     }
+
 }
