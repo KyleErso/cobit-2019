4dce595b8c0eb7298f2a9a959a71d57fb61ffe9f
haidar <89732397+haidarhamda@users.noreply.github.com>
2025-08-04 11:58:01 +0700

    Update show.blade.php

--- resources/views/cobit2019/objectives/show.blade.php
+++ resources/views/cobit2019/objectives/show.blade.php
@@ -110,8 +110,8 @@
         </div>
     </div> --}}
     <div class="bg-white shadow rounded mb-6">
-        <div class="border-b border-gray-200">
-            <nav class="-mb-px flex space-x-8 px-6" aria-label="Tabs">
+        <div class="flex border-b border-gray-200">
+            <nav class="-mb-px space-x-8 px-6 flex-col sm:flex-row" aria-label="Tabs">
                 @php
                     $tabs = ['overview' => 'Overview','goals'=>'Goals','practices'=>'Practices','infoflows'=>'Information Flows','organizational'=>'Organizational','policies'=>'Policies','skills'=>'Skills','culture'=>'Culture & Ethics','services'=>'Services'];
                 @endphp
@@ -129,7 +129,7 @@
                         type="button"
                         class="px-4 py-2 transition-colors duration-200 text-sm font-medium rounded-t-lg"
                         :class="activeComponent === '{{ $key }}' 
-                            ? 'text-red-600 bg-indigo-600 border-b-2 border-indigo-600' 
+                            ? 'text-blue-600 bg-indigo-600 border-b-2 border-indigo-600' 
                             : 'text-gray-500 border-b-2 border-transparent hover:text-gray-700 hover:bg-gray-100 hover:border-gray-300'"
                     >
                         {{ $label }}
@@ -139,6 +139,7 @@
             </nav>
         </div>
     </div>
+    
 
     {{-- Overview Component --}}
     <div x-show="activeComponent === 'overview'" class="component-section">
