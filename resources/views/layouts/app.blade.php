5af8e4180acd9704a8d4e534a5d07735f16a486d
haidar <89732397+haidarhamda@users.noreply.github.com>
2025-08-04 11:16:28 +0700

    component navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <!-- Scripts -->
+    @stack('scripts')
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
   </head>
   <body class="{{ Route::is('login', 'register') ? 'login' : '' }}">
