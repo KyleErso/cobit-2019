02ab9042b00b38f5aeca7227432be7734d9c8b33
haidar <89732397+haidarhamda@users.noreply.github.com>
2025-08-04 11:32:15 +0700

    edit app.blade.php & vite config

--- resources/views/cobit2019/objectives/show.blade.php
+++ resources/views/cobit2019/objectives/show.blade.php
@@ -6,8 +6,11 @@
 @endphp
 
 @extends('layouts.app')
-
 @section('content')
+<head>
+    <title>Objective: {{ $objective->objective_id }} - {{ $objective->objective }}</title>
+    {{-- @vite('resources/css/app.css') --}}
+</head>
 <div class="container mx-auto p-6 mb-8" x-data="{ 
     activeComponent: 'overview',
     init() {
@@ -126,7 +129,7 @@
                         type="button"
                         class="px-4 py-2 transition-colors duration-200 text-sm font-medium rounded-t-lg"
                         :class="activeComponent === '{{ $key }}' 
-                            ? 'text-white bg-indigo-600 border-b-2 border-indigo-600' 
+                            ? 'text-red-600 bg-indigo-600 border-b-2 border-indigo-600' 
                             : 'text-gray-500 border-b-2 border-transparent hover:text-gray-700 hover:bg-gray-100 hover:border-gray-300'"
                     >
                         {{ $label }}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <!-- Scripts -->
     @stack('scripts')
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/app.css', ])
   </head>
   <body class="{{ Route::is('login', 'register') ? 'login' : '' }}">
     <div id="app">
